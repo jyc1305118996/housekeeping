f842bdde1448c929adc4c1f786a32010d7bb098b
CrazyAaronM <hxm520WHL>
2019-10-17 15:15:01 +0800

    通用返回类

---
+++ src/main/java/com/haige/common/bean/Result.java
@@ -0,0 +1,206 @@
+package com.haige.common.bean;
+
+/**
+ * @author : Aaron
+ * create at:  2019-10-17  10:58
+ * @description: 通用返回类
+ */
+public class Result {
+    private int code;
+
+    private String msg;
+
+    private Object data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public enum StateType {
+
+        /**
+         * 成功返回状态
+         */
+
+        OK(200, "OK"),
+
+
+        /**
+         * 请求格式错误
+         */
+
+        BAD_REQUEST(400, "bad request"),
+
+        /**
+         * 未授权
+         */
+
+        UNAUTHORIZED(401, "unauthorized"),
+
+        /**
+         * 没有权限
+         */
+
+        FORBIDDEN(403, "forbidden"),
+
+
+        /**
+         * 请求的资源不存在
+         */
+
+        NOT_FOUND(404, "not found"),
+
+        /**
+         * 该http方法不被允许
+         */
+
+        NOT_ALLOWED(405, "method not allowed"),
+
+        /**
+         * 请求处理发送异常
+         */
+
+        PROCESSING_EXCEPTION(406, "Handling Exceptions"),
+
+        /**
+         * 请求处理未完成
+         */
+
+        PROCESSING_UNFINISHED(407, "To deal with unfinished"),
+
+
+        /**
+         * 登录过期
+         */
+
+        BEOVERDUE(408, "Be overdue"),
+
+
+        /**
+         * 用户未登录
+         */
+
+        NOT_LOGIN(409, "Not logged in"),
+
+
+        /**
+         * 这个url对应的资源现在不可用
+         */
+
+        GONE(410, "gone"),
+
+        /**
+         * 请求类型错误
+         */
+
+        UNSUPPORTED_MEDIA_TYPE(415, "unsupported media type"),
+
+        /**
+         * 校验错误时用
+         */
+
+        UNPROCESSABLE_ENTITY(422, "unprocessable entity"),
+
+        /**
+         * 请求过多
+         */
+
+        TOO_MANY_REQUEST(429, "too many request");
+
+
+        private int code;
+
+        private String value = null;
+
+
+        private StateType(int code, String value) {
+
+            this.code = code;
+
+            this.value = value;
+
+        }
+
+
+        public String value() {
+
+            return this.value;
+
+        }
+
+
+        public int getCode() {
+
+            return code;
+
+        }
+
+        @Override
+        public String toString() {
+            return "StateType{" +
+                    "code=" + code +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
+    }
+
+    public Result(){
+
+    }
+    public Result(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    public Result success(Object data){
+
+        return success(StateType.OK.value,data);
+
+    }
+
+    public Result success(String msg,Object data){
+
+        return new Result(StateType.OK.code,msg,data);
+    }
+
+
+    public Result success(){
+        this.code = StateType.OK.code;
+        this.msg = StateType.OK.value;
+        this.data = null;
+        return this;
+
+    }
+
+}
+
