937289240c4612042e0c159c1502a91d10ddc4ee
CrazyAaronM <hxm520WHL>
2019-10-18 09:13:26 +0800

    通用

---
+++ src/main/java/com/haige/annotation/PermissionType.java
@@ -0,0 +1,25 @@
+package com.haige.annotation;
+
+/**
+ * @author : Aaron
+ * create at:  2019-10-17  21:11
+ * @description: 权限枚举类
+ */
+public enum PermissionType {
+
+    /**
+     * 无需登陆、无需管理员
+     * */
+    ALL,
+    /**
+     * 登陆才可以使用
+     * */
+    LOGIN,
+    /**
+     * 管理员可用
+     * */
+    ADMIM
+
+
+}
+
