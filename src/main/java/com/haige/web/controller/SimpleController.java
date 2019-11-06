0e3c92c25ce4a487ae6b6447d3fcbb8042df7658
CrazyAaronM <hxm520WHL>
2019-11-06 17:50:56 +0800

    文件上传

--- src/main/java/com/haige/web/controller/SimpleController.java
+++ src/main/java/com/haige/web/controller/SimpleController.java
@@ -2,16 +2,21 @@ package com.haige.web.controller;
 
 import com.haige.auth.annotation.Permission;
 import com.haige.auth.enums.PermissionType;
+import com.haige.common.bean.Result;
 import com.haige.common.bean.ResultInfo;
+import com.haige.common.enums.StatusCode;
 import com.haige.service.SmsService;
 import com.haige.service.SystemUserService;
+import com.haige.util.FileUtils;
 import com.haige.web.convert.SmsConvertUtils;
 import com.haige.web.request.SavePersonRequest;
 import com.haige.web.vo.SendSmsRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -29,6 +34,9 @@ public class SimpleController {
 
     @Autowired
     private SystemUserService systemUserService;
+
+    @Autowired
+    private FileUtils fileUtils;
 
     @RequestMapping("/log")
     @Permission(PermissionType.ALL)
@@ -53,9 +61,37 @@ public class SimpleController {
     }
 
     @RequestMapping("testcode/{phone}/{code}")
-    public Mono<ResultInfo<String>> testcode(ServerWebExchange exchange, @PathVariable String phone ,@PathVariable String code) {
+    public Mono<ResultInfo<String>> testcode(ServerWebExchange exchange, @PathVariable String phone, @PathVariable String code) {
         log.info(phone);
 
         return systemUserService.loginByPhoneAndCode(phone, code, exchange);
+    }
+
+    @RequestMapping(value = "/baseUploadFile", method = {RequestMethod.POST})
+    public Mono<ResultInfo<Object>> baseUploadFile(ServerWebExchange exchange,@RequestPart("file") FilePart filePart) {
+
+        System.out.println("1111111");
+
+        return exchange.getSession().map(webSession -> {
+
+
+            try {
+                ResultInfo<Object> resultInfo = new ResultInfo<Object>();
+                resultInfo.setCode(Result.StateType.OK.getCode());
+                resultInfo.setData(fileUtils.uploadFile(filePart));
+                return resultInfo;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResultInfo<Object>(StatusCode.PROCESSING_EXCEPTION);
+            }
+        });
+
+    }
+
+    @RequestMapping("testcode1/{phone}/{code}")
+    public Mono<ResultInfo<String>> testcode1(ServerWebExchange exchange, @PathVariable String phone, @PathVariable String code) {
+        log.info("1x");
+
+        return systemUserService.loginByPhoneAndCode("1", "1", exchange);
     }
 }
