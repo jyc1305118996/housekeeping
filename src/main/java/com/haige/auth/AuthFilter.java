8ae1de06e6b6c26ed7e4aa35a7f5a4a09c537c5d
CrazyAaronM <hxm520WHL>
2019-12-28 23:14:16 +0800

    订单服务状态列表新增字段

--- src/main/java/com/haige/auth/AuthFilter.java
+++ src/main/java/com/haige/auth/AuthFilter.java
@@ -1,71 +1,71 @@
-//package com.haige.auth;
-//
-//import com.haige.common.enums.StatusCodeEnum;
-//import com.haige.service.UserBaseService;
-//import com.haige.service.dto.UserBaseDTO;
-//import com.haige.util.DateUtils;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.http.server.reactive.ServerHttpRequest;
-//import org.springframework.http.server.reactive.ServerHttpResponse;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.server.ServerWebExchange;
-//import org.springframework.web.server.WebFilter;
-//import org.springframework.web.server.WebFilterChain;
-//import reactor.core.publisher.Mono;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-///**
-// * @author Archie
-// * @date 2019/10/20 18:26
-// */
-//@Order(10)
-//@Component
-//@Slf4j
-//public class AuthFilter implements WebFilter {
-//
-//
-//    @Autowired
-//    private UserBaseService userBaseService;
-//
-//    @Override
-//    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-//        ServerHttpRequest request = exchange.getRequest();
-//        ServerHttpResponse response = exchange.getResponse();
-//        // 非登陆和短信全过滤
-//        boolean isLogin = !request.getPath().value().contains("/login");
-//        boolean isSms = !request.getPath().value().contains("/sms/send");
-//        boolean isGoods = !request.getPath().value().contains("/goods");
-//        if (isLogin && isSms && isGoods){
-//            response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
-//            List<String> auth = request.getHeaders().get("Authorization");
-//            if (auth == null || auth.isEmpty()){
-//                return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.NOT_LOGIN.getCode() + "\", \"message\":\"" + StatusCodeEnum.NOT_LOGIN.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
-//            }
-//            try{
-//                UserBaseDTO userBaseDTO= userBaseService.findByToken(auth.get(0));
-//                // 未登录
-//                if (userBaseDTO == null){
-//                    return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.NOT_LOGIN.getCode() + "\", \"message\":\"" + StatusCodeEnum.NOT_LOGIN.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
-//                }
-//                String expreDate = userBaseDTO.getUbdTokenExpreDate();
-//                LocalDateTime dateTime = DateUtils.convertToDateTime(expreDate);
-//                // 过期
-//                if (dateTime.isBefore(LocalDateTime.now())){
-//                    return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.BEOVERDUE.getCode() + "\", \"message\":\"" + StatusCodeEnum.BEOVERDUE.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
-//                }
-//                // 登陆通过，将用户放入session域
-//                exchange.getAttributes().put("user", userBaseDTO);
-//            }catch (Exception e){
-//                // 未认证非法请求
-//                return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.NOT_LOGIN.getCode() + "\", \"message\":\"" + StatusCodeEnum.NOT_LOGIN.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
-//            }
-//        }
-//
-//        // 认证通过
-//        return chain.filter(exchange);
-//    }
-//}
+package com.haige.auth;
+
+import com.haige.common.enums.StatusCodeEnum;
+import com.haige.service.UserBaseService;
+import com.haige.service.dto.UserBaseDTO;
+import com.haige.util.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Archie
+ * @date 2019/10/20 18:26
+ */
+@Order(10)
+@Component
+@Slf4j
+public class AuthFilter implements WebFilter {
+
+
+    @Autowired
+    private UserBaseService userBaseService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        // 非登陆和短信全过滤
+        boolean isLogin = !request.getPath().value().contains("/login");
+        boolean isSms = !request.getPath().value().contains("/sms/send");
+        boolean isGoods = !request.getPath().value().contains("/goods");
+        if (isLogin && isSms && isGoods){
+            response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+            List<String> auth = request.getHeaders().get("Authorization");
+            if (auth == null || auth.isEmpty()){
+                return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.NOT_LOGIN.getCode() + "\", \"message\":\"" + StatusCodeEnum.NOT_LOGIN.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
+            }
+            try{
+                UserBaseDTO userBaseDTO= userBaseService.findByToken(auth.get(0));
+                // 未登录
+                if (userBaseDTO == null){
+                    return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.NOT_LOGIN.getCode() + "\", \"message\":\"" + StatusCodeEnum.NOT_LOGIN.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
+                }
+                String expreDate = userBaseDTO.getUbdTokenExpreDate();
+                LocalDateTime dateTime = DateUtils.convertToDateTime(expreDate);
+                // 过期
+                if (dateTime.isBefore(LocalDateTime.now())){
+                    return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.BEOVERDUE.getCode() + "\", \"message\":\"" + StatusCodeEnum.BEOVERDUE.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
+                }
+                // 登陆通过，将用户放入session域
+                exchange.getAttributes().put("user", userBaseDTO);
+            }catch (Exception e){
+                // 未认证非法请求
+                return response.writeWith(Mono.just(response.bufferFactory().wrap(("{\"code\":\"" + StatusCodeEnum.NOT_LOGIN.getCode() + "\", \"message\":\"" + StatusCodeEnum.NOT_LOGIN.getValue() + "\", \"data\":\"\", \"count\":\"\"}").getBytes())));
+            }
+        }
+
+        // 认证通过
+        return chain.filter(exchange);
+    }
+}
